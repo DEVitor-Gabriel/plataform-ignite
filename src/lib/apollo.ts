62ffc58330bdc2ba829b113f17196e8b01d8609d
Vitor Gabriel <vitorg.34825@gmail.com>
2022-07-01 22:00:24 -0300

    tela subscribe

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-
+// console.log(import.meta.env.VITE_API_ACCESS_TOKEN)
 export const client = new ApolloClient({
-    uri: "https://api-eu-west-2.graphcms.com/v2/cl4ywteie0dim01t82n3sd5db/master",
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
